223bc707bf3f45770cb28740b9f35fe3506b697b
Vitalii900 <melnikvitaliy30@gmail.com>
2022-12-10 23:54:45 +0200

    finished task

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ const countryInfoRef = document.querySelector('.country-info');
 inputRef.addEventListener('input', debounce(findCountry, DEBOUNCE_DELAY));
 
 function findCountry(event) {
-  fetchCountries(event.target.value).then(addCountriesMurkup);
+  fetchCountries(event.target.value.trim(' '))
+    .then(addCountriesMurkup);
 }
 
 function addCountriesMurkup(countries) {
@@ -22,26 +23,36 @@ function addCountriesMurkup(countries) {
   } else if (countries.length > 1 && countries.length <= 10) {
     countryInfoRef.innerHTML = '';
     countriesList(countries);
-  } else Notify.info('Too many matches found. Please enter a more specific name.');
+  } else if (countries.status === 404) {
+    Notify.failure('Oops, there is no country with that name');
+  } else if (!countries.length) {
+    countryListRef.innerHTML = '';
+    countryInfoRef.innerHTML = '';
+  } else {
+    Notify.info('Too many matches found. Please enter a more specific name.')
+    countryListRef.innerHTML = '';
+    countryInfoRef.innerHTML = '';
+  };
 }
 
 function countriesList(items) {
   const murkupFromArray = items.map(item => {
     return `<li class="flagAndName"><img class="countres_flag" src="${item.flags.svg}">
-    <p class="country_title">${item.name.common}</p></li>`;
+    <p class="country_text"><span class="">${item.name.common}</span></p></li>`;
   }).join('');
   countryListRef.innerHTML = murkupFromArray;
 }
-
 
 function countriesCard(countries) {
   const singleMurkup = countries.map(item => {
     return `<div class="flagAndName">
     <img class="countres_flag" src="${item.flags.svg}">
-    <p>${item.name.common}</p></div>
-    <p>Capital: ${item.capital}</p>
-    <p>Population: ${item.population}</p>
-    <p>Languages: ${Object.values(item.languages)}</p>`;
+    <p class="country_text">${item.name.common}</p></div>
+    <p><span class="country_title">Capital: </span>${item.capital}</p>
+    <p><span class="country_title">Population: </span>${item.population}</p>
+    <p><span class="country_title">Languages: </span>${Object.values(
+      item.languages
+    )}</p>`;
   });
   countryInfoRef.innerHTML = singleMurkup;
 }
